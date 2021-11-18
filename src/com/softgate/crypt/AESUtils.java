375ac77822173e7ff75a39105fea63e4b0f7f0bb
mark-jay <mark.jay.mk@gmail.com>
2021-11-19 05:29:14 +0600

    Update AESUtils.java

--- src/com/softgate/crypt/AESUtils.java
+++ src/com/softgate/crypt/AESUtils.java
@@ -12,12 +12,57 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
+import javax.crypto.Cipher;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Map;
+
+
 /**
  * A simple utility class for easily encrypting and decrypting data using the AES algorithm.
  * 
  *  @author Chad Adams
  */
 public class AESUtils {
+	
+	{
+		String errorString = "Failed manually overriding key-length permissions.";
+		int newMaxKeyLength;
+		try {
+		    if ((newMaxKeyLength = Cipher.getMaxAllowedKeyLength("AES")) < 256) {
+			Class c = Class.forName("javax.crypto.CryptoAllPermissionCollection");
+			Constructor con = c.getDeclaredConstructor();
+			con.setAccessible(true);
+			Object allPermissionCollection = con.newInstance();
+			Field f = c.getDeclaredField("all_allowed");
+			f.setAccessible(true);
+			f.setBoolean(allPermissionCollection, true);
+
+			c = Class.forName("javax.crypto.CryptoPermissions");
+			con = c.getDeclaredConstructor();
+			con.setAccessible(true);
+			Object allPermissions = con.newInstance();
+			f = c.getDeclaredField("perms");
+			f.setAccessible(true);
+			((Map) f.get(allPermissions)).put("*", allPermissionCollection);
+
+			c = Class.forName("javax.crypto.JceSecurityManager");
+			f = c.getDeclaredField("defaultPolicy");
+			f.setAccessible(true);
+			Field mf = Field.class.getDeclaredField("modifiers");
+			mf.setAccessible(true);
+			mf.setInt(f, f.getModifiers() & ~Modifier.FINAL);
+			f.set(null, allPermissions);
+
+			newMaxKeyLength = Cipher.getMaxAllowedKeyLength("AES");
+		    }
+		} catch (Exception e) {
+		    throw new RuntimeException(errorString, e);
+		}
+		if (newMaxKeyLength < 256)
+		    throw new RuntimeException(errorString); // hack failed	
+	}
 
 	/**
 	 * The constant that denotes the algorithm being used.
@@ -39,7 +84,7 @@ public class AESUtils {
 	public static SecretKey generateKey() {
 		try {
 			KeyGenerator keyGenerator = KeyGenerator.getInstance(algorithm);
-			keyGenerator.init(128);
+			keyGenerator.init(256);
 			return keyGenerator.generateKey();
 		} catch (Exception ex) {
 
